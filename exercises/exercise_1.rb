7728a54a38737fc86bcdd0adc8060a9883caa3f5
ankitaahuja <ankitaahuja111@gmail.com>
2018-10-09 23:53:39 +0000

    All exercises Done

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -11,3 +11,4 @@ Store.create(name: "Burnaby",annual_revenue: 300000, mens_apparel: true, womens_
 Store.create(name: "Richmond",annual_revenue: 1260000, mens_apparel: false, womens_apparel: true )
 Store.create(name: "Gastown",annual_revenue: 190000, mens_apparel: true, womens_apparel: false )
 
+
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,10 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter a store name"
+store_name = gets.chomp
+NewStore = Store.create(
+    name: store_name
+)
+
+puts NewStore.errors.full_messages
