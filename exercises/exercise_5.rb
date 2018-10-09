0e7f9dd5377089e36aa4402506d8faa17bd8a629
ankitaahuja <ankitaahuja111@gmail.com>
2018-10-09 22:06:20 +0000

    exercises 2-6 DOne

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@sum = Store.sum(:annual_revenue)
+puts @sum
+@averageRevenue = @sum/5
+puts @averageRevenue
+
+# User.where("zip_code = '48104'").group('users.id').count.length
+# Output the number of stores that are generating $1M or more in annual sales
+@NoOfStores = Store.where("annual_revenue > 1000000").count
+puts @NoOfStores
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -7,5 +7,13 @@ require_relative './exercise_5'
 
 puts "Exercise 6"
 puts "----------"
-
+@store1 = Store.find_by(id:1)
+@store2 = Store.find_by(id:2)
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Nagpal", last_name: "Avanya", hourly_rate: 70)
+@store1.employees.create(first_name: "Ahuja", last_name: "Ankita", hourly_rate: 90)
+@store1.employees.create(first_name: "Monga", last_name: "Anita", hourly_rate: 90)
+@store2.employees.create(first_name: "Malhotra", last_name: "Meenu", hourly_rate: 40)
+@store2.employees.create(first_name: "Jackson", last_name: "David", hourly_rate: 50)
+@store2.employees.create(first_name: "Pal", last_name: "Sonu", hourly_rate: 70)
